cb6a8d70caec880cb0e8ef3c8d6225051cbe5438
Margo Art <margoart.work@gmail.com>
2017-10-21 20:16:15 -0500

    corrected mistake

--- Main.js
+++ Main.js
@@ -2,7 +2,7 @@ let input = 'Whale talk is fun!';
 let vowels = ['a', 'e', 'i', 'o', 'u'];
 let resultArray = [];
 for (let inputIndex = 0; inputIndex < input.length; inputIndex ++) {
-    for (let vowelsIndex; vowelsIndex < vowels.length; vowelsIndex ++) {
+    for (let vowelsIndex = 0; vowelsIndex < vowels.length; vowelsIndex ++) {
         if (input[inputIndex] === vowels[vowelsIndex]) {
             resultArray.push(input[inputIndex]);
         }
